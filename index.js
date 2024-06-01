df75f66000beddf373f5424a743458be03ae884f
mukundangopalachary <mukundangopalachary@gmail.com>
2024-06-01 16:13:18 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 let count = 0;
-
+let savel = document.getElementById("save-element")
 let countel = document.getElementById("count-element")
 
 function increment(){
     count+=1;
-    countel.innerText = count;
+    countel.textContent = count;
     console.log("The button was clicked!")
 }
+
+function save(){
+    let countstr = count + " - "
+    savel.textContent += countstr
+    console.log(count)
+    count = 0;
+    countel.textContent = count;
+}
+
+
